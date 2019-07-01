403d433684634d7ff0dfb4d3be8a1521ca2d5c9d
Matteo Barbieri <neopard@gmail.com>
2019-07-01 18:32:17 +0200

    Add files for map class and test level generation (WIP)

---
+++ src/test_maps.cpp
@@ -0,0 +1,129 @@
+
+#include "GameMap.hpp"
+
+/*
+def generate_dungeon_level(width, height, min_room_length, max_room_length):
+    # TODO add parameters (and use them!)
+
+    #############################
+    ######### FIN QUI ###########
+    #############################
+
+    # Collect coordinates in a variable
+    xy = [xc-10, yc-6, xc+10, yc+6]
+
+    entry_room = Room(xy, list(Direction))
+    level.add_part(entry_room)
+
+    # Add an external layer of walls to rooms
+    logging.getLogger().info("Adding walls")
+    add_walls(level)
+
+    # Populate Dungeon with entities
+    # Monsters
+    add_monsters(level)
+
+    # Add some items in the room
+    add_items(level)
+
+    # Create and add entry stairs '<'
+    entry_x, entry_y = entry_room.center
+
+    up_stairs_component = Stairs(level.dungeon_level - 1)
+    up_stairs = Entity(
+        entry_x, entry_y, '<',
+        libtcod.white, 'Stairs up', render_order=RenderOrder.STAIRS,
+        components=dict(stairs=up_stairs_component))
+
+    level.entities.append(up_stairs)
+
+    return level
+*/
+
+//GameMap generate_dungeon_level(width, height, min_room_length, max_room_length)
+GameMap generate_dungeon_level(int width, int height)
+{
+
+    GameMap level(width, height);
+
+    // Coordinates of map center
+    int xc = width/2;
+    int yc = height/2;
+
+    return level;
+}
+
+/*
+"""
+Create a dungeon-like map level, using tunnellers
+"""
+
+from ..game_map import GameMap, Room
+
+from ..tile import Tile, Wall
+
+from components.stairs import Stairs
+
+from ..directions import Direction
+
+import logging
+
+import random
+
+# import libtcodpy as libtcod
+import tcod as libtcod
+
+from render_functions import RenderOrder
+
+from entity import Entity
+
+from prefabs.orc import make_orc
+from prefabs.weapons.pistol import make_pistol
+
+from components.ai import MonsterAi
+
+def add_walls(level):
+    """
+    Creates walls in all Tile-type tiles adjacent to something non-Tile
+    """
+
+    for X in range(level.width):
+        for Y in range(level.height):
+            for x in range(max(X-1, 0), min(X+2, level.width)):
+                for y in range(max(Y-1, 0), min(Y+2, level.height)):
+                    if (type(level.tiles[X][Y]) == Tile and \
+                       (type(level.tiles[x][y]) not in [Tile, Wall])):
+                       ## Create wall
+                       level.tiles[X][Y] = Wall.create_from_palette()
+
+def add_monsters(level):
+    """
+    Add stairs for previous and next dungeon level
+    """
+
+    for room in level.rooms:
+        # TODO for now, just generate an orc in every room
+        if random.random() < 2:
+
+            monster = make_orc(room, MonsterAi)
+
+            level.entities.append(monster)
+
+
+def add_items(level):
+    """
+    Add a ranged weapon on the floor
+    """
+
+    x = int(level.width/2) - 2
+    y = int(level.height/2) - 2
+
+    pistol = make_pistol()
+    pistol.x = x
+    pistol.y = y
+
+    level.entities.append(pistol)
+
+
+
+*/
