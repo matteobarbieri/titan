d0a4e22a01b4bbde7ca8cb3cb10349462971f6fc
Matteo Barbieri <neopard@gmail.com>
2019-07-24 12:28:24 +0200

    Fix bug on fov map computations
    `walkable` and `transparent` properties were inverted.

--- src/fov_functions.cpp
+++ src/fov_functions.cpp
@@ -18,10 +18,13 @@ TCODMap * initialize_fov(GameMap * game_map)
             tile_index = compute_tile_index(x, y, game_map->width);
 
             // Set tile properties
+            // blocks and blocks_sight must be negated, as the fov properties
+            // correspond to their opposites (`walkable` and `transparent`
+            // respectively)
             fov_map->setProperties(
                 x, y,
-                (game_map->tiles[tile_index])->block_sight(),
-                (game_map->tiles[tile_index])->blocked());
+                !(game_map->tiles[tile_index])->block_sight(),
+                !(game_map->tiles[tile_index])->blocked());
         }
     }
 
