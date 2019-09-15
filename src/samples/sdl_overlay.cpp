2a17739125c7a5e8a3654032c185a7524fc1d79b
Matteo Barbieri <neopard@gmail.com>
2019-09-15 14:32:53 +0200

    Implement working stupid sdl effects (WIP)

--- src/samples/sdl_overlay.cpp
+++ src/samples/sdl_overlay.cpp
@@ -13,12 +13,226 @@
 #include "../sdl/timer.hpp"
 #include "../sdl/text.hpp"
 
-const int SCREEN_FPS = 60;
+const int SCREEN_FPS = 100;
+//const int SCREEN_FPS = 5;
 const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
 //#include "sdl/custom_renderer.cpp"
+bool sdl_callback_enabled = false;
 
 using namespace std;
+
+class SampleRenderer : public ITCODSDLRenderer {
+public :
+	SampleRenderer() : effectNum(0), delay(3.0f) {
+		noise=new TCODNoise(3);
+	}
+	~SampleRenderer() {
+		delete noise;
+	}
+	void render(void *sdlSurface) {
+		SDL_Surface *screen = (SDL_Surface *)sdlSurface;
+		// now we have almighty access to the screen's precious pixels !!
+		// get the font character size
+		int charw,charh;
+		TCODSystem::getCharSize(&charw,&charh);
+		// compute the sample console position in pixels
+		int samplex = 0 * charw;
+		int sampley = 0 * charh;
+		delay -= TCODSystem::getLastFrameLength();
+		if ( delay < 0.0f ) {
+			delay = 3.0f;
+			effectNum = (effectNum + 1) % 3;
+			if ( effectNum == 2 ) sdl_callback_enabled=false; // no forced redraw for burn effect
+			else sdl_callback_enabled=true;
+		}
+		switch(effectNum) {
+			case 0 : blur(screen,samplex,sampley,SCREEN_WIDTH * charw,SCREEN_HEIGHT * charh); break;
+			case 1 : explode(screen,samplex,sampley,SCREEN_WIDTH * charw,SCREEN_HEIGHT * charh); break;
+			case 2 : burn(screen,samplex,sampley,SCREEN_WIDTH * charw,SCREEN_HEIGHT * charh); break;
+		}
+	}
+
+protected :
+	TCODNoise *noise;
+	int effectNum;
+	float delay;
+
+	void burn(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
+		int ridx=screen->format->Rshift/8;
+		int gidx=screen->format->Gshift/8;
+		int bidx=screen->format->Bshift/8;
+		for (int x=samplex; x < samplex + samplew; x ++ ) {
+			uint8_t *p = (uint8_t *)screen->pixels + x * screen->format->BytesPerPixel + sampley * screen->pitch;
+			for (int y=sampley; y < sampley + sampleh; y ++ ) {
+				int ir=0,ig=0,ib=0;
+				uint8_t *p2 = p + screen->format->BytesPerPixel; // get pixel at x+1,y
+				ir += p2[ridx];
+				ig += p2[gidx];
+				ib += p2[bidx];
+				p2 -= 2*screen->format->BytesPerPixel; // get pixel at x-1,y
+				ir += p2[ridx];
+				ig += p2[gidx];
+				ib += p2[bidx];
+				p2 += screen->format->BytesPerPixel+ screen->pitch; // get pixel at x,y+1
+				ir += p2[ridx];
+				ig += p2[gidx];
+				ib += p2[bidx];
+				p2 -= 2*screen->pitch; // get pixel at x,y-1
+				ir += p2[ridx];
+				ig += p2[gidx];
+				ib += p2[bidx];
+				ir/=4;
+				ig/=4;
+				ib/=4;
+				p[ridx]=ir;
+				p[gidx]=ig;
+				p[bidx]=ib;
+				p += screen->pitch;
+			}
+		}
+	}
+
+	void explode(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
+		int ridx=screen->format->Rshift/8;
+		int gidx=screen->format->Gshift/8;
+		int bidx=screen->format->Bshift/8;
+		TCODRandom *rng=TCODRandom::getInstance();
+		int dist=(int)(10*(3.0f - delay));
+		for (int x=samplex; x < samplex + samplew; x ++ ) {
+			uint8_t *p = (uint8_t *)screen->pixels + x * screen->format->BytesPerPixel + sampley * screen->pitch;
+			for (int y=sampley; y < sampley + sampleh; y ++ ) {
+				int ir=0,ig=0,ib=0;
+				for (int i=0; i < 3; i++) {
+					int dx = rng->getInt(-dist,dist);
+					int dy = rng->getInt(-dist,dist);
+					uint8_t *p2;
+					p2 = p + dx * screen->format->BytesPerPixel;
+					p2 += dy * screen->pitch;
+					ir += p2[ridx];
+					ig += p2[gidx];
+					ib += p2[bidx];
+				}
+				ir/=3;
+				ig/=3;
+				ib/=3;
+				p[ridx]=ir;
+				p[gidx]=ig;
+				p[bidx]=ib;
+				p += screen->pitch;
+			}
+		}
+	}
+
+	void blur(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
+		// let's blur that sample console
+		float f[3],n=0.0f;
+		int ridx=screen->format->Rshift/8;
+		int gidx=screen->format->Gshift/8;
+		int bidx=screen->format->Bshift/8;
+		f[2]=TCODSystem::getElapsedSeconds();
+		for (int x=samplex; x < samplex + samplew; x ++ ) {
+			uint8_t *p = (uint8_t *)screen->pixels + x * screen->format->BytesPerPixel + sampley * screen->pitch;
+			f[0]=(float)(x)/samplew;
+			for (int y=sampley; y < sampley + sampleh; y ++ ) {
+				int ir=0,ig=0,ib=0;
+				if ( (y-sampley)%8 == 0 ) {
+					f[1]=(float)(y)/sampleh;
+					n=noise->getFbm(f,3.0f);
+				}
+				int dec = (int)(3*(n+1.0f));
+				int count=0;
+				switch(dec) {
+					case 4:
+						count += 4;
+						// get pixel at x,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= 2*screen->format->BytesPerPixel; // get pixel at x+2,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= 2*screen->pitch; // get pixel at x+2,y+2
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p+= 2*screen->format->BytesPerPixel; // get pixel at x,y+2
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += 2*screen->pitch;
+					case 3:
+						count += 4;
+						// get pixel at x,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += 2*screen->format->BytesPerPixel; // get pixel at x+2,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += 2*screen->pitch; // get pixel at x+2,y+2
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p-= 2*screen->format->BytesPerPixel; // get pixel at x,y+2
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= 2*screen->pitch;
+					case 2:
+						count += 4;
+						// get pixel at x,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= screen->format->BytesPerPixel; // get pixel at x-1,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= screen->pitch; // get pixel at x-1,y-1
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p+= screen->format->BytesPerPixel; // get pixel at x,y-1
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += screen->pitch;
+					case 1:
+						count += 4;
+						// get pixel at x,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += screen->format->BytesPerPixel; // get pixel at x+1,y
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p += screen->pitch; // get pixel at x+1,y+1
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p-= screen->format->BytesPerPixel; // get pixel at x,y+1
+						ir += p[ridx];
+						ig += p[gidx];
+						ib += p[bidx];
+						p -= screen->pitch;
+						ir/=count;
+						ig/=count;
+						ib/=count;
+						p[ridx]=ir;
+						p[gidx]=ig;
+						p[bidx]=ib;
+					break;
+					default:break;
+				}
+				p += screen->pitch;
+			}
+		}
+	}
+};
 
 
 //Rendered texture
@@ -150,6 +364,9 @@ void render_all(TCODConsole * a)
 
 int main(int argc, char *argv[])
 {
+
+    TCODSystem::setFps(30);
+
     TCODConsole::root->setDefaultBackground(TCODColor::black);
 
     TCODConsole * a = new TCODConsole(20, 20);
@@ -187,11 +404,16 @@ int main(int argc, char *argv[])
     SDL_Renderer * renderer = TCOD_sys_get_SDL_renderer();
     SDL_Window * sdl_window = TCOD_sys_get_SDL_window();
 
+    bool ccc = (SDL_GetRenderer(sdl_window) == renderer);
+
     bool bbb = renderer == NULL;
     bool aaa = sdl_window == NULL;
-    DEBUG(bbb);
-    DEBUG(aaa);
+    DEBUG(aaa << " (should be 0)");
+    DEBUG(bbb << " (should be 0)");
+    DEBUG(ccc << " (should be 1)");
     DEBUG("After!");
+
+    //return 1;
 
     SDL_Rect sdl_rect;
 
@@ -226,13 +448,12 @@ int main(int argc, char *argv[])
     //TCOD_sys_get_sdl_window();
     //TCOD_sys_accumulate_console(messages_layer);
 
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    //SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
     while (!TCODConsole::root->isWindowClosed())
     {
 
         render_all(a);
-        TCODConsole::root->flush();
 
         //Start cap timer
         capTimer.start();
@@ -249,6 +470,17 @@ int main(int argc, char *argv[])
                 play_game_ = true;
                 break;
             case 'b':
+                sdl_callback_enabled = !sdl_callback_enabled;
+
+                if (sdl_callback_enabled) {
+                    DEBUG("SDL ENABLED!");
+                    TCODSystem::registerSDLRenderer(new SampleRenderer());
+                } else {
+                    DEBUG("SDL DISABLED!");
+                    TCODSystem::registerSDLRenderer(NULL);
+                    // we want libtcod to redraw the sample console even if nothing has changed in it
+                    TCODConsole::root->setDirty(0, 0, SCREEN_WIDTH , SCREEN_HEIGHT);
+                }
                 load_game = true;
                 break;
             case 'c':
@@ -276,33 +508,48 @@ int main(int argc, char *argv[])
         rr++;
         DEBUG(rr);
 
+        /*
         if (rr > 20)
         {
-        sdl_rect.x = rr % 200;
-        sdl_rect.y = rr % 200;
+            sdl_rect.x = rr % 200;
+            sdl_rect.y = rr % 200;
 
-        //SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF );
-        SDL_RenderClear(renderer);
+            //SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF );
+            SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 127);
-        SDL_RenderFillRect(renderer, &sdl_rect);
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 127);
+            SDL_RenderFillRect(renderer, &sdl_rect);
 
-        int frameTicks = capTimer.getTicks();
-        if( frameTicks < SCREEN_TICKS_PER_FRAME )
-        {
-            //Wait remaining time
-            SDL_Delay( SCREEN_TICKS_PER_FRAME - frameTicks );
+            int frameTicks = capTimer.getTicks();
+            if( frameTicks < SCREEN_TICKS_PER_FRAME )
+            {
+                //Wait remaining time
+                //SDL_Delay( SCREEN_TICKS_PER_FRAME - frameTicks );
+                //SDL_Delay(1000);
+            }
+
+            DEBUG("Step 1");
+            SDL_Delay(1000);
+            //Render current frame
+            loadMedia(renderer);
+            gTextTexture.render( 300, 300 , renderer);
+
+            DEBUG("Step 2");
+            
+            //SDL_Delay(1000);
+            TCODConsole::root->flush();
+            //TCODConsole::root->setDirty(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            SDL_Delay(1000);
+            SDL_RenderPresent(renderer);
+            //DEBUG("Presenting!");
+            DEBUG("Step 3");
+            SDL_Delay(1000);
+            DEBUG("Step 4");
         }
+        */
 
-        //Render current frame
-        loadMedia(renderer);
-        gTextTexture.render( 300, 300 , renderer);
-
-        SDL_RenderPresent(renderer);
-        //DEBUG("Presenting!");
-        }
-
+        TCODConsole::flush();
         //TCODConsole::root->flush();
 
     }
--- src/sdl/text.cpp
+++ src/sdl/text.cpp
@@ -46,21 +46,9 @@ std::string textureText, SDL_Color textColor, TTF_Font * gFont, SDL_Renderer * g
 /*This source code copyrighted by Lazy Foo' Productions (2004-2019)
 and may not be redistributed without written permission.*/
 
-//Using SDL, SDL_image, SDL_ttf, standard IO, math, and strings
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
 
 //Frees media and shuts down SDL
 void close();
