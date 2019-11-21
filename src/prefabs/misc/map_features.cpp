017ec350e85d3d8f713538cd372eac253c1bd09a
Matteo Barbieri <neopard@gmail.com>
2019-11-21 10:15:33 +0100

    Complete test for doors unlock

--- src/prefabs/misc/map_features.cpp
+++ src/prefabs/misc/map_features.cpp
@@ -65,22 +65,7 @@ Entity * make_terminal(int x, int y, TCODColor symbol_color, std::string termina
 
      //Add the interactive component to the panel
     InteractiveTerminal * interactive_component = new InteractiveTerminal(
-            is_active);
-
-    // TODO these are sample functions, which should not be added here
-    TerminalFunction tf1("Test 1", 'a', test_f1);
-    TerminalFunction tf2("Test 2", 'b', test_f2);
-
-    auto test_f3 = [](Entity * player, GameMap * game_map, GameState * game_state)
-    {
-        std::cout << "This is test function (lambda) f3 from a terminal" << std::endl;
-    };
-
-    TerminalFunction tf3("Test 3 (lambda)", 'c', test_f3);
-
-    interactive_component->terminal_functions.push_back(tf1);
-    interactive_component->terminal_functions.push_back(tf2);
-    interactive_component->terminal_functions.push_back(tf3);
+        is_active);
 
     terminal->interactive = interactive_component;
 
--- src/prefabs/misc/map_features.hpp
+++ src/prefabs/misc/map_features.hpp
@@ -8,13 +8,34 @@
 class Entity;
 class Direction;
 
-Entity * make_door(int x, int y, bool open=false, bool locked=false, unsigned int key_id=0);
+/**
+ * Creates a door, an entity which blocks sight and can be opened (if unlocked).
+ */
+Entity * make_door(int x, int y,
+                   bool open=false, bool locked=false,
+                   unsigned int key_id=0);
 
-Entity * make_text_panel(int x, int y, Direction * readable_from, std::string text,
-                         TCODColor symbol_color=TCODColor::darkGreen, TCODColor=TCODColor::lightAzure, int symbol=240,
+/**
+ * Creates a text panel which will display information in the player's log when
+ * bumped into. The panel will be readable from one direction only, this way
+ * they can be placed directly into walls.
+ */
+Entity * make_text_panel(int x, int y, Direction * readable_from,
+                         std::string text,
+                         TCODColor symbol_color=TCODColor::darkGreen,
+                         TCODColor text_color=TCODColor::lightAzure,
+                         int symbol=240,
                          bool is_active=true);
 
-Entity * make_terminal(int x, int y, TCODColor symbol_color=TCODColor::darkGreen, std::string terminal_name="Terminal",
-                       int symbol=167, bool is_active=true);
+/**
+ * Creates a terminal, an entity with which the player can interact and operate.
+ * By default it has no functionalities, they must be added individually
+ * somewhere else.
+ */
+Entity * make_terminal(int x, int y,
+                       TCODColor symbol_color=TCODColor::darkGreen,
+                       std::string terminal_name="Terminal",
+                       int symbol=167,
+                       bool is_active=true);
 
 #endif /* ifndef ROGUE_20177_PREFABS_MISC_MAP_FEATURES */
