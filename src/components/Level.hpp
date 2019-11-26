120bea2b09491eb573b38c16d65750188bc7d26a
Matteo Barbieri <neopard@gmail.com>
2019-11-26 14:28:02 +0100

    Refactor Level.{h,c}pp

--- src/components/Level.hpp
+++ src/components/Level.hpp
@@ -15,9 +15,6 @@ class Level
 
         Entity * owner;
 
-        Level();
-        ~Level();
-
         /**
          * Creates a json representation of the component
          */
@@ -26,29 +23,4 @@ class Level
         static Level * from_json(json j);
 };
 
-
 #endif /* ifndef ROGUE_20177_LEVEL */
-
-/*
-class Level:
-    def __init__(self, current_level=1, current_xp=0, level_up_base=200, level_up_factor=150):
-        self.current_level = current_level
-        self.current_xp = current_xp
-        self.level_up_base = level_up_base
-        self.level_up_factor = level_up_factor
-
-    @property
-    def experience_to_next_level(self):
-        return self.level_up_base + self.current_level * self.level_up_factor
-
-    def add_xp(self, xp):
-        self.current_xp += xp
-
-        if self.current_xp > self.experience_to_next_level:
-            self.current_xp -= self.experience_to_next_level
-            self.current_level += 1
-
-            return True
-        else:
-            return False
-*/
