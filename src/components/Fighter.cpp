8034d6685381137b53fab0ec311e7b5ae7cf76e1
Matteo Barbieri <neopard@gmail.com>
2019-12-11 11:03:32 +0100

    Change damage message for melee attacks

--- src/components/Fighter.cpp
+++ src/components/Fighter.cpp
@@ -22,7 +22,9 @@
 
 #include "../GameMessages.hpp"
 
+// Prototypes for auxiliary functions
 bool weapon_in_range(Entity * attacker, Entity * target, Entity * weapon, WeaponAttack * weapon_attack);
+bool ammo_check(Entity * weapon);
 
 Fighter::Fighter(int max_hp, int hp, int _fighting, int _accuracy) :
     _max_hp(max_hp), _hp(hp), _fighting(_fighting), _accuracy(_accuracy)
@@ -185,17 +187,13 @@ bool Fighter::attack_with_melee_weapon(Entity * target, Entity * weapon, WeaponA
         int amount = (
             rand() % (weapon_attack->dmg_delta() + 1)) + weapon_attack->dmg_min;
 
-        // TODO reduce amount based on target's armour
         amount = target->fighter->apply_damage_reduction(amount);
 
         target->fighter->take_damage(amount);
 
-        // TODO
-        // Change message based on who's attacking (Player or NPC)
-        stringStream << "You punch the " << 
-            target->name << " in the face for " << amount << " damage!";
-
         // Add message to message log
+        stringStream << target->name << " was hit for " << amount << " damage [" << weapon->name << "]";
+        
         MessageLog::singleton().add_message(
             {stringStream.str(), TCODColor::white});
     }
@@ -242,9 +240,9 @@ bool Fighter::attack_with_ranged_weapon(Entity * target, Entity * weapon, Weapon
         // Apply damage to target
         target->fighter->take_damage(amount);
 
+        // Add message to message log
         stringStream << target->name << " was hit for " << amount << " damage [" << weapon->name << "]";
 
-        // Add message to message log
         MessageLog::singleton().add_message(
             {stringStream.str(), TCODColor::white});
 
@@ -418,124 +416,3 @@ bool Fighter::is_dead() const
 {
     return _hp <= 0;
 }
-
-/*
-from render_functions import RenderOrder
-
-from .item import ItemType, ItemSubtype
-
-class NoMeleeWeaponsEquippedException(Exception):
-    pass
-
-
-class NoRangedWeaponsEquippedException(Exception):
-    pass
-
-
-class Fighter:
-
-    def get_adjusted_stat(self, stat_name):
-        base_stat = self.stats['base_{}'.format(stat_name)]
-
-        if self.owner and self.owner.equipment:
-            equipment_bonus = self.owner.equipment.stat_bonus(stat_name)
-        else:
-            equipment_bonus = 0
-
-        return base_stat + equipment_bonus
-
-    @property
-    def INT(self):
-        return self.get_adjusted_stat('int')
-
-    @property
-    def max_hp(self):
-        if self.owner and self.owner.equipment:
-            bonus = self.owner.equipment.max_hp_bonus
-        else:
-            bonus = 0
-
-        return self.base_max_hp + bonus
-
-    @property
-    def power(self):
-        if self.owner and self.owner.equipment:
-            bonus = self.owner.equipment.power_bonus
-        else:
-            bonus = 0
-
-        return self.base_power + bonus
-
-    @property
-    def defense(self):
-        if self.owner and self.owner.equipment:
-            bonus = self.owner.equipment.defense_bonus
-        else:
-            bonus = 0
-
-        return self.base_defense + bonus
-
-    def heal(self, amount):
-        self.hp += amount
-
-        if self.hp > self.max_hp:
-            self.hp = self.max_hp
-
-    def shoot(self, target):
-        """
-        other : Entity
-        """
-
-        messages = list()
-
-        equipped_ranged_weapons = list()
-
-        # First check that whoever is shooting actually has a ranged weapon
-        # equipped
-
-        for _, w in self.owner.equipment.slots.items():
-            if (w and ItemType.WEAPON in w.item.item_types and
-                ItemSubtype.RANGED in w.item.item_subtypes):
-
-                # It is an equipped ranged weapon
-                equipped_ranged_weapons.append(w)
-
-        # If no ranged weapons are equipped, raise an exception
-        if len(equipped_ranged_weapons) == 0:
-            raise NoRangedWeaponsEquippedException()
-
-        # Check if weapon has enough ammo to shoot
-        # TODO implement
-
-        # Check if enemy is in weapon range
-        # TODO implement
-        weapons_in_range = equipped_ranged_weapons
-
-        # Shoot with remaining viable weapons
-        # TODO implement
-
-        for w in weapons_in_range:
-            if not self.roll_to_hit_ranged(target, w):
-                # TODO better implement this
-                messages.append(Message("Missed!", libtcod.yellow))
-
-            damage = self.calculate_damage(target, w)
-
-            if damage > 0:
-
-                messages.append(
-                    Message('{0} shoots {1} for {2} hit points.'.format(
-                        self.owner.name.capitalize(), target.name,
-                        str(damage)), libtcod.white))
-
-                # In case there are other effects, add extra messages
-                messages.extend(target.fighter.take_damage(damage))
-            else:
-
-                # Append the no damage dealt messages
-                messages.append(
-                    Message('{0} attacks {1} but does no damage.'.format(
-                        self.owner.name.capitalize(), target.name), libtcod.white))
-
-        return messages
-*/
